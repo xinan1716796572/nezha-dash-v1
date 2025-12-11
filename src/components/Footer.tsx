a0e9b24a51cd5f364b0b7d4722877638b6a8ba00
心安 <71492391+xinan1716796572@users.noreply.github.com>
2025-12-11 20:17:37 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-// import { fetchSetting } from "@/lib/nezha-api"
-// import { useQuery } from "@tanstack/react-query"
 import React from "react"
 import { useTranslation } from "react-i18next"
 
@@ -7,18 +5,12 @@ const Footer: React.FC = () => {
   const { t } = useTranslation()
   const isMac = /macintosh|mac os x/i.test(navigator.userAgent)
 
-  // const { data: settingData } = useQuery({
-  //   queryKey: ["setting"],
-  //   queryFn: () => fetchSetting(),
-  //   refetchOnMount: true,
-  //   refetchOnWindowFocus: true,
-  // })
-
   return (
     <footer className="mx-auto w-full max-w-5xl px-4 lg:px-0 pb-4 server-footer">
       <section className="flex flex-col">
         <section className="mt-1 flex items-center sm:flex-row flex-col justify-between gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50 server-footer-name">
           <div className="flex items-center gap-1">
+            <p></p>
           </div>
           <div className="server-footer-theme flex flex-col items-center sm:items-end">
             <p className="mt-1 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
@@ -26,12 +18,13 @@ const Footer: React.FC = () => {
                 {isMac ? <span className="text-xs">⌘</span> : "Ctrl "}K
               </kbd>
             </p>
+            <section>
+              {t("footer.themeBy")}
             </section>
           </div>
         </section>
       </section>
     </footer>
-  )
-}
+)
 
 export default Footer
